9aa6087f78499303649b5d73a4e8ab2858417389
kevin12172010 <131439004+kevin12172010@users.noreply.github.com>
2023-05-26 16:50:12 +0900

    aaaaa
    aaaa

---
+++ 0526 최정우(연산자).py
@@ -0,0 +1,32 @@
+#계산기 프로그램
+#조건문
+#if 조건
+#elif 조건
+#else:
+'''
+a=float(input("a를 입력하세요:"))
+b=float(input("b를 입력하세요:"))
+op=input("연산자를 입력하세요:")
+if op=='+':
+    c=a+b
+    print(f"a+b={c}")
+elif op=='-':
+    c=a-b
+    print(f"a-b={c}")
+elif op=='/':
+    c=a/b
+    print(f"a/b={c}")
+elif op=='*':
+    c=a*b
+    print(f"a*b={c}")
+else:
+    print("연산자 오류")
+'''
+#음양수 판별 프로그램
+c=int(input("숫자를 입력하세요:"))
+if c>0:
+    print("양수")
+elif c<0:
+    print("음수")
+else:0
+print("정수")
